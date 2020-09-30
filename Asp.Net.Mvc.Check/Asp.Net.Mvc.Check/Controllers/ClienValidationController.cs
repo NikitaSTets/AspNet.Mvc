52bb6277ea27cbdad8d6e721972f1a8bd9506e28
Nikita stets <mikita.stets.999@mail.ru>
2020-09-30 23:32:55 +0300

    ClientSide Validation

---
+++ Asp.Net.Mvc.Check/Asp.Net.Mvc.Check/Controllers/ClienValidationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Asp.Net.Mvc.Check.Controllers
+{
+    public class ClienValidationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.Net.Mvc.Check/Asp.Net.Mvc.Check/Models/ClientModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.Net.Mvc.Check.Models
+{
+    public class ClientModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
